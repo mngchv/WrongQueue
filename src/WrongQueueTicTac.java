9ae13d1a717c2aee1187db71c47798c3a3a5c536
Rustam Mingachev <mingachevr@mail.ru>
2021-05-15 12:18:37 +0300

    Done(Correct read and record)

---
+++ src/WrongQueueTicTac.java
@@ -0,0 +1,93 @@
+public class WrongQueueTicTac {
+
+    public static void main(String[] args) {
+        new WrongQueueTicTac().go();
+    }
+
+    private void go() {
+
+        MyQ myQ = new MyQ();
+
+        new Producer(myQ);
+        new Consumer(myQ);
+
+    }
+
+
+    class Producer implements Runnable {
+
+        public MyQ queue;
+
+        public Producer(MyQ queue) {
+            this.queue = queue;
+            new Thread(this, "Producer").start();
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+
+            while (true) {
+                queue.put(i++);
+            }
+        }
+    }
+
+    class Consumer implements Runnable {
+
+        public MyQ queue;
+
+        public Consumer(MyQ queue) {
+            this.queue = queue;
+            new Thread(this, "Consumer").start();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                queue.get();
+            }
+        }
+    }
+    class MyQ
+    {
+        volatile int n;
+        volatile boolean isSend = false;
+
+        synchronized void put(int n)
+        {
+            while(isSend)
+            {
+                try
+                {
+                    wait();
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            this.n = n;
+            System.out.println("Tic.");
+            isSend = true;
+            notify();
+        }
+
+        synchronized int get()
+        {
+            while(!isSend)
+            {
+                try
+                {
+                    wait();
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Tac.");
+            isSend = false;
+            notify();
+            return n;
+        }
+    }
+}
